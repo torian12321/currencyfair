6148f98f151500cafb89cf0efa3aeec84f91ab17
Aitor PC <aitorpalomares@hotmail.com>
2019-09-04 20:02:25 +0100

    header, change size

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import classNames from "classnames";
 import { useWindowScrollPosition } from "components/hooks";
-// import logo from "./logo.svg";
 import styles from "./Header.module.scss";
+import logo from "./cf_logo.svg";
 
 const Header: React.FC = () => {
   const { positionY } = useWindowScrollPosition();
-  const isSmall = positionY > 100;
+  const isSmall = positionY > 60;
 
   return (
     <header
       className={classNames(styles.header, isSmall && styles.header__small)}
     >
-      The logo goes here - {positionY}
+      <div className={styles.header_content}>
+        <img className={styles.header_logo} src={logo} alt="CurrencyFair" />
+      </div>
     </header>
   );
 };
