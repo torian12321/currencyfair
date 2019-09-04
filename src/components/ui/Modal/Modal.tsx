bd68ba3326498af9ffc50d278a8d4acd642c6edd
Aitor <apalomares@mi9retail.com>
2019-09-04 17:00:47 +0100

    modal: first version

--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -1,29 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { useEventListener, useKeyPress } from "components/hooks";
+import { useKeyPress } from "components/hooks";
 // import classNames from 'classnames';
 // import { ISpinner } from "./Spinner.interfaces";
-// import styles from './Spinner.module.scss';
+import styles from "./Modal.module.scss";
 
 const KEY_ESC = 27;
 
-const Modal = () => {
+const Modal = (props: any) => {
+  const { onDismiss = () => {} } = props;
+  const [isVisible, setIsVisible] = useState(true);
+
   const escIsPressed = useKeyPress(KEY_ESC);
-  const onKeyDown = ({ keyCode }: any) => {
-    // const { isVisible } = props;
-    if (keyCode === KEY_ESC) {
-      console.log("close this");
+
+  useEffect(() => {
+    bodyAddClassName();
+
+    return () => {
+      bodyDelClassName();
+    };
+  }, []);
+
+  const bodyAddClassName = () =>
+    document.body.classList.add(styles.body_hasModal);
+  const bodyDelClassName = () =>
+    document.body.classList.remove(styles.body_hasModal);
+
+  useEffect(() => {
+    if (isVisible) {
+      bodyAddClassName();
+    } else {
+      bodyDelClassName();
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    escIsPressed && closeModal();
+  }, [escIsPressed]);
+
+  const closeModal = () => {
+    if (isVisible) {
+      onDismiss();
+      setIsVisible(false);
     }
   };
 
-  useEffect(() => {
-    console.log("We are pressing scape");
-  }, [escIsPressed]);
-
-  useEventListener("keydown", onKeyDown, document);
-
   return ReactDOM.createPortal(
-    <div>ABC {escIsPressed ? "AAA" : "BBB"} </div>,
+    isVisible && (
+      // !escIsPressed && (
+      <>
+        <div className={styles.modal_BG} />
+        <div className={styles.modal}>
+          <div onClick={closeModal}>close me</div>
+        </div>
+      </>
+    ),
     document.body
   );
 };
