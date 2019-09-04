125ee387dbdd0d03e697186a0e9f38fa1d83920a
Aitor <apalomares@mi9retail.com>
2019-09-04 17:54:37 +0100

    Updates

--- precache-manifest.3e5e616a606b9f2d4b4b0025ee14e0b2.js
+++ precache-manifest.3e5e616a606b9f2d4b4b0025ee14e0b2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbe7e202e0fb576e45b2675a2457d7c7",
+    "revision": "0872c23d7d917b0af453295cbe0f3641",
     "url": "/currencyfair/index.html"
   },
   {
-    "revision": "89faf05c6fda23567ae9",
-    "url": "/currencyfair/static/css/main.2e6a46ec.chunk.css"
+    "revision": "e7119af36c2b8bc51a8b",
+    "url": "/currencyfair/static/css/main.c7639b07.chunk.css"
   },
   {
-    "revision": "0fe269b441a875b53e5c",
-    "url": "/currencyfair/static/js/2.d825da98.chunk.js"
+    "revision": "1476f976d89102136432",
+    "url": "/currencyfair/static/js/2.70aa5517.chunk.js"
   },
   {
-    "revision": "89faf05c6fda23567ae9",
-    "url": "/currencyfair/static/js/main.09308664.chunk.js"
+    "revision": "e7119af36c2b8bc51a8b",
+    "url": "/currencyfair/static/js/main.6ebf8311.chunk.js"
   },
   {
     "revision": "e5e1729f938d8928a208",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/currencyfair/precache-manifest.ce1681a779ae50a4e95f81d2f21efd3d.js"
+  "/currencyfair/precache-manifest.3e5e616a606b9f2d4b4b0025ee14e0b2.js"
 );
 
 self.addEventListener('message', (event) => {
